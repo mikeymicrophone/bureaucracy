f84470700fa68532c0640f3870549c013a54c4c1
Mike Schwab <mike.schwab@gmail.com>
2009-09-05 17:57:57 -0400

    removed destroy links for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users, :rooms, :belongings, :buildings, :placements, :storage_units, :storages
+  map.resources :rooms do |room|
+    room.resources :belongings
+  end
   map.resource :user_session
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
